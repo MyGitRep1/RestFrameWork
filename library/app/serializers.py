6594ff82d10ece774136702106bf420676f5b2c7
MyGitRep1 <106673455+MyGitRep1@users.noreply.github.com>
2023-01-18 09:56:52 +0300

    Lesson 3 (#3)
    * fix
    
    * Migrations and serializers
    
    * Added views and urls

--- library/app/serializers.py
+++ library/app/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Author
+from .models import Author, Book, Article, Biography
 
 
 class AuthorModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Author
+        fields = '__all__'
+
+
+class BiographyModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Biography
+        fields = '__all__'
+
+
+class ArticleModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class BookModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Book
         fields = '__all__'
