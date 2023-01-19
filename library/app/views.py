af0fc888bb46e85a40ebcfa262c994ed9f6748c2
MyGitRep1 <uzinovev26@gmail.com>
2023-01-19 15:10:21 +0300

    lesson_4 Code fix

--- library/app/views.py
+++ library/app/views.py
@@ -1,4 +1,9 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet, ViewSet
 from .models import Author, Book, Article, Biography
 from .serializers import AuthorModelSerializer, BookModelSerializer, ArticleModelSerializer, BiographyModelSerializer
 
@@ -6,6 +11,13 @@ from .serializers import AuthorModelSerializer, BookModelSerializer, ArticleMode
 class AuthorModelViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
+
+    def get_queryset(self):
+        param = self.request.query_params.get('name')
+        print(self.request.query_params)
+        if param:
+            return Author.objects.filter(first_name__contains=param[0])
+        return super().get_queryset()
 
 
 class BookModelViewSet(ModelViewSet):
@@ -21,4 +33,17 @@ class ArticleModelViewSet(ModelViewSet):
 class BiographyModelViewSet(ModelViewSet):
     queryset = Biography.objects.all()
     serializer_class = BiographyModelSerializer
+
+
+class MyAPIView(ViewSet):
+
+    def list(self, request):
+        authors = Author.objects.all()
+        serializer = AuthorModelSerializer(authors, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def babayka(self, request):
+        return Response({'daata': 'RATATA'})
+
 
