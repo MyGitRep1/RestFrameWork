7d914aa45488efcf62b636476d89c746837ed2e5
MyGitRep1 <uzinovev26@gmail.com>
2023-01-22 18:34:16 +0300

    added Switch, Redirect

--- library/frontend/src/App.js
+++ library/frontend/src/App.js
@@ -4,7 +4,16 @@ import logo from './logo.svg';
 import './App.css';
 import AuthorList from './components/Author.js'
 import BookList from "./components/Books";
-import {HashRouter, Route, Link} from 'react-router-dom'
+import {HashRouter, Route, Link, Switch, Redirect} from 'react-router-dom'
+
+const NotFound404 = ({location}) => {
+    return (
+       <div>
+           <h1>Страница по адресу `{location.pathname}' не найдена</h1>
+       </div>
+    )
+}
+
 
 class App extends React.Component {
     constructor(props) {
@@ -52,8 +61,12 @@ class App extends React.Component {
                            </li>
                         </url>
                     </nav>
-                    <Route exact path='/' component={() => <AuthorList authors={this.state.authors} />} />
-                    <Route exact path='/books' component={() => <BookList items={this.state.books} />} />
+                    <Switch>
+                        <Route exact path='/' component={() => <AuthorList authors={this.state.authors} />} />
+                        <Route exact path='/books' component={() => <BookList items={this.state.books} />} />
+                        <Redirect from='/authors' to='/'/>
+                        <Route component={NotFound404}/>
+                    </Switch>
                 </HashRouter>
            </div>
        )
