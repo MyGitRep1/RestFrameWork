cd89fc60942aeca614664cf2d5a575b599d516e6
MyGitRep1 <uzinovev26@gmail.com>
2023-01-29 21:38:16 +0300

    Created: 'Auth.js'

--- library/frontend/src/App.js
+++ library/frontend/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import AuthorList from './components/Author.js'
 import BookList from "./components/Books";
 import {HashRouter, Route, Link, Switch, Redirect} from 'react-router-dom'
+import AuthorBookList from "./AuthorBook";
+import LoginForm from "./Auth";
 
 const NotFound404 = ({location}) => {
     return (
@@ -19,32 +21,37 @@ class App extends React.Component {
     constructor(props) {
          super(props)
 
-         const author1 = {id: 1, first_name: 'Грин', birthday_year: 1880}
-         const author2 = {id: 2, first_name: 'Пушкин', birthday_year: 1790}
-         const author = [author1, author2]
-
-         const book1 = {id: 1, name: 'Алые паруса', author: author1}
-         const book2 = {id: 2, name: 'Золотая цепь', author: author1}
-         const book3 = {id: 3, name: 'Пиковая дама', author: author2}
-         const book4 = {id: 4, name: 'Капитанская дочка', author: author2}
-         const books = [book1, book2, book3, book4]
-
          this.state = {
-             'authors': authors,
-             'books': books
+             'authors': [],
+             'books': []
          }
     }
 
+    load_data() {
+        axios.get('http://127.0.0.1:8002/api/authors')
+            .then(response => {
+                const authors = response.data
+                    this.setState(
+                          {
+                        'authors': authors['results']
+                    }
+                )
+            }).catch(error => console.log(error))
+
+        axios.get('http://127.0.0.1:8002/api/books')
+            .then(response => {
+                const books = response.data['results']
+                    this.setState(
+                          {
+                        'books': books
+                    }
+                )
+            }).catch(error => console.log(error))
+
+    }
+
     componentDidMount() {
-       axios.get('http://127.0.0.1:8000/api/authors')
-           .then(response => {
-               const authors = response.data
-                   this.set.State(
-                         {
-                       'authors': authors
-                   }
-               )
-           }).catch(error => console.log(error))
+         this.load_data()
     }
 
     render () {
@@ -56,8 +63,11 @@ class App extends React.Component {
                            <li>
                               <Link to='/'>Authors</Link>
                            </li>
-                           <li>
+                            <li>
                               <Link to='/books'>Books</Link>
+                           </li>
+                           <li>
+                              <Link to='/login'>Login</Link>
                            </li>
                         </url>
                     </nav>
@@ -65,6 +75,7 @@ class App extends React.Component {
                         <Route exact path='/' component={() => <AuthorList authors={this.state.authors} />} />
                         <Route exact path='/books' component={() => <BookList items={this.state.books} />} />
                         <Route exact path='/author/:id' component={() => <AuthorBookList items={this.state.books} />} />
+                        <Route exact path='/login' component={() => <LoginForm />} />
                         <Redirect from='/authors' to='/'/>
                         <Route component={NotFound404}/>
                     </Switch>
