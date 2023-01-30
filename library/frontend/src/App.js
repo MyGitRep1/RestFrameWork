797851cc0eb11e5b738b8507d3aa6e9a8e1a5833
MyGitRep1 <uzinovev26@gmail.com>
2023-01-30 14:24:14 +0300

    Added in App.js

--- library/frontend/src/App.js
+++ library/frontend/src/App.js
@@ -27,8 +27,51 @@ class App extends React.Component {
          }
     }
 
+    set_token(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        localStorage.setItem('token', token)
+        this.setState({'token': token}, () => this.load_data())
+    }
+
+    is_authenticated() {
+        return this.state.token != ''
+    }
+
+    logout () {
+        this.set_token('')
+    }
+
+    get_token_from_storage() {
+         const cookies = new Cookies()
+         // const token = cookies.get('token')
+         const taken = localStorage.getItem('token')
+         this.setState({'token': token}, () => this.load_data())
+    }
+
+
+    get_token(login,password) {
+        axios.post('http://127.0.0.1:8002/api-token-auth/, {username: login, password: password')}
+            .then(response => {
+                this.set_token(response.data['taken'])
+            })catch(error => alert('Неверный пароль'))
+    }
+
+    get_headers() {
+        let headers = {
+            'Content_Type': 'application?json',
+         }
+         if (this.is_authenticated())
+         {
+             headers['Authorization'] = 'Token ' + this.state.token
+         }
+         return headers
+    }
+
+
     load_data() {
-        axios.get('http://127.0.0.1:8002/api/authors')
+        const headers = this.get_headers()
+        axios.get('http://127.0.0.1:8002/api/authors', {headers})
             .then(response => {
                 const authors = response.data
                     this.setState(
@@ -38,7 +81,7 @@ class App extends React.Component {
                 )
             }).catch(error => console.log(error))
 
-        axios.get('http://127.0.0.1:8002/api/books')
+        axios.get('http://127.0.0.1:8002/api/books', {headers})
             .then(response => {
                 const books = response.data['results']
                     this.setState(
@@ -51,7 +94,7 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-         this.load_data()
+         this.get_token_from_storage()
     }
 
     render () {
@@ -67,7 +110,7 @@ class App extends React.Component {
                               <Link to='/books'>Books</Link>
                            </li>
                            <li>
-                              <Link to='/login'>Login</Link>
+                               {<this.is_authenticated() ? <button onClick={() => this.logout()}>Logout</button> : <Link to='?login'>Login</Link>}
                            </li>
                         </url>
                     </nav>
@@ -75,7 +118,7 @@ class App extends React.Component {
                         <Route exact path='/' component={() => <AuthorList authors={this.state.authors} />} />
                         <Route exact path='/books' component={() => <BookList items={this.state.books} />} />
                         <Route exact path='/author/:id' component={() => <AuthorBookList items={this.state.books} />} />
-                        <Route exact path='/login' component={() => <LoginForm />} />
+                        <Route exact path='/login' component={() => <LoginForm get_token={(login, password) => this.get_token(login, password)}/>} />
                         <Redirect from='/authors' to='/'/>
                         <Route component={NotFound404}/>
                     </Switch>
