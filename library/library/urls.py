df938726e85ce0a4714f0eef62384a4353de77ce
MyGitRep1 <uzinovev26@gmail.com>
2023-02-06 12:34:33 +0300

    Created urls.py in app

--- library/library/urls.py
+++ library/library/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from app.views import AuthorModelViewSet, ArticleModelViewSet, BiographyModelViewSet, BookModelViewSet, MyAPIView)
 from rest_framework.authtoken.views import obtain_auth_token
@@ -14,8 +14,12 @@ router.register('my', MyAPIView, basename='my')
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/', include(router.urls)),
-    path('api/', include(router.urls)),
+   # path('admin/', admin.site.urls),
+   # path('api-auth/', include('rest_framework.urls')),
+   # path('api/', include(router.urls)),
+   # path('api/', include(router.urls)),
+   # re_path(r'^myapi/(?P<version>\d)/authors/$', MyAPIView.as_view({'get': 'list'})),
+   path('api/1/authors', include('authors.urls', namespace='1')),
+   path('api/2/authors', include('authors.urls', namespace='2')),
+
 ]
